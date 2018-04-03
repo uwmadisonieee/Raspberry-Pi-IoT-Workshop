74f1f4835b34690981905d7fac49d37188367f08
sjfricke <spencerfricke@gmail.com>
2018-04-03 07:03:48 +0000

    working example

--- server.py
+++ server.py
@@ -1,5 +1,4 @@
-
-from flask import Flask
+from flask import Flask, send_from_directory
 import RPi.GPIO as GPIO
 
 # Setup LED
@@ -7,18 +6,23 @@ LED = 7
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(LED, GPIO.OUT)
 
-# Sets our server as 'app'
-app = Flask(__name__)
+app = Flask(__name__, static_url_path="")
+app._static_folder = "static"
 
 # Creates route for LED
 @app.route('/LED/<int:status>')
-def index(status):
+def led(status):
     if status == 0:
-		GPIO.output(LED, GPIO.LOW)
-	elif status == 1:
-		GPIO.output(LED, GPIO.HIGH)
+	GPIO.output(LED, GPIO.LOW)
+    elif status == 1:
+	GPIO.output(LED, GPIO.HIGH)
 
     return 'LED Light status set to: ' + str(status)
+
+@app.route('/')
+def root():
+	print "TEST"
+	return app.send_static_file('index.html')
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
